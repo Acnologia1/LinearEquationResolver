08764b828ba22c9da701d304e1c4531d590a2bfa
vancong <tavancong1994@gmail.com>
2019-05-10 09:12:39 +0700

    Create LinearEquationResolver.java
    LinearEquationResolver

---
+++ LinearEquationResolver.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a: ");
+        double a = scanner.nextDouble();
+        System.out.print("b: ");
+        double b = scanner.nextDouble();
+        System.out.print("c: ");
+        double c = scanner.nextDouble();
+        if (a != 0) {
+            double solution = -b / a;
+            System.out.printf("The solution is: %f!", solution);
+        } else {
+            if (b == 0) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+}
